0e2516689729e4bf4c96b2dc84af6345e12a2c40
samir <samir@SamirPC>
2024-02-05 20:50:07 -0300

    First Commit

---
+++ controle-empresa/src/main/java/com/cyrix/ControleEmpresaApplication.java
@@ -0,0 +1,13 @@
+package com.cyrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleEmpresaApplication.class, args);
+	}
+
+}
---
+++ controle-empresa/src/test/java/com/cyrix/ControleEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleEmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
