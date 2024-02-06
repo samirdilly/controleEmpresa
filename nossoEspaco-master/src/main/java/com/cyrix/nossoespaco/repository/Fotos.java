69d7f3b89f183e2484e83d685e18734f484b06be
Samir Dilly <95063884+samirdilly@users.noreply.github.com>
2024-02-05 21:06:20 -0300

    Delete nossoEspaco-master directory

--- nossoEspaco-master/src/main/java/com/cyrix/nossoespaco/repository/Fotos.java
+++
@@ -1,9 +0,0 @@
-package com.cyrix.nossoespaco.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cyrix.nossoespaco.model.Foto;
-
-public interface Fotos extends JpaRepository<Foto, Long> {
-
-}
